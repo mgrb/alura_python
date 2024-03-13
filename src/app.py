caaefcb685fadebfc6d37357b478c417ee0a71a2
Marlos Ribeiro <marlos.mgrb@gmail.com>
2024-03-13 18:54:23 -0300

    Cria a base da aplicação de estudo

---
+++ src/app.py
@@ -0,0 +1,9 @@
+print('SABOR EXPRESS \n')
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair \n')
+
+opc = input('Escolha uma opção:')
+
+print('Você escolheu a opção:', opc)
