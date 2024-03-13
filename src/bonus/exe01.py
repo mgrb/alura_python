de6d7867a56c6b8de4ea1fe8db0ee14bcd407781
Marlos Ribeiro <marlos.mgrb@gmail.com>
2024-03-13 19:15:58 -0300

    Atividades do modulo de manipulação de Strings

---
+++ src/bonus/exe01.py
@@ -0,0 +1,13 @@
+import math
+
+
+print('Python na Escola de Programação da Alura!\n')
+nome = 'Fulano' #input('Qual é o seu nome? ')
+idade = 30 #input('Qual é a sua idade? ')
+
+print(f'Olá {nome}, você tem {idade} anos de idade.\n')
+
+print('A','L','U','R','A',sep='\n')
+
+pi = math.pi
+print(f'\nO valor de pi é: {round(pi,2)}')
